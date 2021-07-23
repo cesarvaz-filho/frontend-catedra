9c8c43b38506578542ac866dc9de0079ce7e687f
César Augusto Vaz Sampaio Filho <cesar_augusto_perfil@hotmail.com>
2021-07-23 13:52:27 -0300

    add input csv function

---
+++ public/assets/js/create_student.js
@@ -0,0 +1,21 @@
+async function postRequest(url, data) {
+  const response = await fetch(url, {
+    credentials: 'same-origin',
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    }),
+  });
+  return await response.json();
+}
+
+function post() {
+  const data = document.querySelector('.input_csv').value;
+
+  postRequest('https://api-catedra.herokuapp.com/register', data)
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+
+  alert(`O arquivo ${name} foi registrado.`)
+}
